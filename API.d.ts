78c3d9bf2dc3c3c66d4f68843768a077b6faa5cd
Peter Ingram <p.ingram@managedracks.co.uk>
2020-03-09 20:38:20 +0000

    tidy typings

--- API.d.ts
+++ API.d.ts
@@ -1,33 +1,46 @@
-export interface GetResponse {
-    name: string;
-    error: (string | null);
-    type: string; // image | text | web
-    data: (null | object | string);
+export enum Actions {
+    INFO = 'info',
+    LOG = 'log',
+    STORAGE_GET = 'storage.get',
+    STORAGE_PUT = 'storage.put',
+    STORAGE_DELETE = 'storage.delete',
+    STORAGE_LIST = 'storage.list',
+    STORAGE_CLEAR = 'storage.clear',
+    STORAGE_DOWNLOAD = 'storage.download',
+    STORAGE_EXISTS = 'storage.exists'
 }
 
-export interface DownloadResponse {
-    url: string;
-    data: (string | null);
-    error: (string | null);
-}
+export type mediaType = 'image' | 'text' | 'web'
 
 interface APIMessageBase {
-    action: 'info' | 'log' | 'storage.get' | 'storage.put' | 'storage.delete' | 'storage.list' | 'storage.clear' | 'storage.download' | 'storage.exists'
+    action: Actions
     name?: string
-}
-
-export interface APIMessage extends APIMessageBase {
-    response?: any
     url?: string
 }
 
-export interface APIMessageInbound extends APIMessageBase {
-    data?: any
-    url?: string
-}
-
-export interface InfoCB {
+interface InfoCB {
     deviceId: string
     version: string
     provider: string
+}
+
+export interface GetResponse {
+    name: string | undefined
+    error: string | null
+    type: mediaType | null
+    data: null | object | string
+}
+
+export interface DownloadResponse {
+    url: string | null | undefined
+    data: string | null
+    error: string | null
+}
+
+export interface OutboundMessage extends APIMessageBase {
+    response: InfoCB | DownloadResponse | GetResponse | boolean | string[] | number
+}
+
+export interface InboundMessage extends APIMessageBase {
+    data: any
 }
