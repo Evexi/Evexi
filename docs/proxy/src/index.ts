5b52dda167a00ca44588aea02bc767d359b2b676
Peter Ingram <p.ingram@mrx.technology>
2021-10-07 09:20:57 +0100

    typo

--- docs/proxy/src/index.ts
+++ docs/proxy/src/index.ts
@@ -30,7 +30,7 @@ async function request() {
     try {
         const res = await window.Evexi.proxy('/square', {
             method: 'POST',
-            body: JSON.stringify({ fod: 'bar' })
+            body: JSON.stringify({ foo: 'bar' })
         })
         if (res && res.ok) log.success(`REQUEST Success: ${JSON.stringify(res)}`)
         else if (res && !res.ok) log.success(`REQUEST Not Ok: ${JSON.stringify(res)}`)
