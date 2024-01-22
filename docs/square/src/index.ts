8f1437c9f07d4ec08d5fee95560aab6a67c384be
peter ingram <peter.ingram0@gmail.com>
2024-01-22 10:58:52 +0700

    better example for square testing

--- docs/square/src/index.ts
+++ docs/square/src/index.ts
@@ -1,8 +1,24 @@
 import { log } from '../../common'
 import Evexi from 'evexi'
 
-log.info('loaded')
+window.playing = async (item) => {
 
-Evexi.square.event(msg => {
-  log.success(JSON.stringify(msg))
-})
+  log.info('loaded' + JSON.stringify(item))
+
+  try {
+    
+    Evexi ? log.success('API Found') : log.error('API ERROR - does not exist')
+
+    // Check we have the required env vars
+    log.success(`ENVIRONMENT: ${await Evexi.env('ENVIRONMENT')}`)
+    log.success(`LOCATION: ${await Evexi.env('LOCATION')}`)
+    log.success(`TERMINAL_ID: ${await Evexi.env('ENVIRONMENT')}`)
+
+    Evexi.square.event(msg => {
+      log.success(JSON.stringify(msg))
+    })
+
+  } catch (e) {
+    log.error('API ERROR - caught')
+  }
+}
