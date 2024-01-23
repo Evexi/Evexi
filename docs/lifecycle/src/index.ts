fb8dba3df0bb3dcd1997c172888c543cc24a159b
peter ingram <peter.ingram0@gmail.com>
2024-01-23 09:20:48 +0700

    small adjustment

--- docs/lifecycle/src/index.ts
+++ docs/lifecycle/src/index.ts
@@ -1,10 +1,10 @@
 import Evexi from 'evexi'
 import { log } from '../../common'
 
-window.playing = (item) => log.info('WINDOW PLAYING -- ' + JSON.stringify(item))
-
-window.stopping = (item) => log.info('WINDOW STOPPING -- ' + JSON.stringify(item))
-
-Evexi.lifecycle.playing(msg => log.info('EVEXI PLAYING -- '+ JSON.stringify(msg)))
-
+// New
+Evexi.lifecycle.playing(msg => log.info('EVEXI PLAYING -- ' + JSON.stringify(msg)))
 Evexi.lifecycle.stopping(msg => log.info('EVEXI STOPPING -- ' + JSON.stringify(msg)))
+
+// Old
+window.playing = (item) => log.info('WINDOW PLAYING -- ' + JSON.stringify(item))
+window.stopping = (item) => log.info('WINDOW STOPPING -- ' + JSON.stringify(item))
