66bd9ca624e9efd199087759ecf1279736ddd61d
Andrei <andrei.cherciu24@gmail.com>
2025-08-27 11:37:38 +0100

    PIP Overlay Demo

--- .build/build.sh
+++ .build/build.sh
@@ -70,5 +70,6 @@ buildZip docs/nexmosphere/src/index.html nexmosphere
 buildZip docs/lifecycle/src/index.html lifecycle
 buildZip docs/square/src/index.html square
 buildZip docs/printer/src/index.html printer
+buildZip docs/pipOverlayDemo/src/index.html pipOverlayDemo
 
 rm -r .tmp
